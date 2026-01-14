6402309b8e90891039fb6c8fc5460d1625c0a2d0
Francisco Entony <franciscoentonydev@gmail.com>
2026-01-14 20:11:12 -0300

    Fix: removed invalid htmlFor from div in Checkbox

--- frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__e910fbfd._.js
+++ frontend/.next/dev/server/chunks/ssr/[root-of-the-server]__e910fbfd._.js
@@ -135,7 +135,6 @@ const Checkbox = ({ anotacao, id })=>{
                 columnNumber: 7
             }, ("TURBOPACK compile-time value", void 0)),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$to$2d$do$2d$list$2f$frontend$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                htmlFor: id,
                 className: "h-5 w-5 flex rounded-md border border-[#a2a1a833] bg-[#e8e8e8] peer-checked:bg-[#0c9096] transition",
                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$to$2d$do$2d$list$2f$frontend$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("svg", {
                     fill: "none",
