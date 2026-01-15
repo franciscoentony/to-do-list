6f1a9c4f44db02c0bb11b38a6c671c6e5e5b10b5
Francisco Entony <franciscoentonydev@gmail.com>
2026-01-15 17:44:28 -0300

    corringindo bugs

--- frontend/app/components/FormLogin.tsx
+++ frontend/app/components/FormLogin.tsx
@@ -12,7 +12,7 @@ export default function FormLogin() {
   async function handleLogin(e: React.FormEvent) {
     e.preventDefault();
 
-    const response = await fetch("http://127.0.0.1:8000/api/token/", {
+    const response = await fetch("https://to-do-list-api-wxwe.onrender.com/api/token/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
