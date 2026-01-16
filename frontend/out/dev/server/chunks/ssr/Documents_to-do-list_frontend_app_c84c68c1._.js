c44b59fd10e2bf0e26ce71e58b9eb273000e2d02
Francisco Entony <franciscoentonydev@gmail.com>
2026-01-15 21:38:54 -0300

    Favicon

---
+++ frontend/out/dev/server/chunks/ssr/Documents_to-do-list_frontend_app_c84c68c1._.js
@@ -0,0 +1,22 @@
+module.exports = [
+"[project]/Documents/to-do-list/frontend/app/icon.png (static in ecmascript, tag client)", ((__turbopack_context__) => {
+
+__turbopack_context__.v("/to-do-list/_next/static/media/icon.de8f21f4.png");}),
+"[project]/Documents/to-do-list/frontend/app/icon.png.mjs { IMAGE => \"[project]/Documents/to-do-list/frontend/app/icon.png (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$to$2d$do$2d$list$2f$frontend$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__ = __turbopack_context__.i("[project]/Documents/to-do-list/frontend/app/icon.png (static in ecmascript, tag client)");
+;
+const __TURBOPACK__default__export__ = {
+    src: __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$to$2d$do$2d$list$2f$frontend$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
+    width: 1000,
+    height: 1000
+};
+}),
+];
+
+//# sourceMappingURL=Documents_to-do-list_frontend_app_c84c68c1._.js.map
